b35808af376405e8b33d709080c77648ee54e5cb
h4ck3r9717 <shawnnjoga@gmail.com>
2024-01-13 20:12:14 +0300

    initial bash learning

---
+++ input.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+echo What is your name?
+read first_name
+echo What is your last name
+read last_name
+
+echo Hello $first_name $last_name 
+
---
+++ variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## BETTER 
+first_name=Shawn
+second_name=Njoga
+
+echo $first_name $second_name
